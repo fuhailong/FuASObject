30eb5304676110a4005dc2292a24ebb002c4f2d3
fuhailong <165255032@qq.com>
2018-06-20 18:05:46 +0800

    Add files via upload

--- FuASYYWebImageManager.m
+++ FuASYYWebImageManager.m
@@ -12,23 +12,25 @@
 
 - (id)downloadImageWithURL:(NSURL *)URL callbackQueue:(dispatch_queue_t)callbackQueue downloadProgress:(ASImageDownloaderProgress)downloadProgress completion:(ASImageDownloaderCompletion)completion
 {
-    YYWebImageManager *manager = [YYWebImageManager sharedManager];
-    __weak YYWebImageOperation *operation = nil;
-    operation = [manager requestImageWithURL:URL
-                                     options:YYWebImageOptionSetImageWithFadeAnimation
-                                    progress:^(NSInteger receivedSize, NSInteger expectedSize) {
-                                        dispatch_async(callbackQueue, ^{
-                                            CGFloat received = receivedSize;
-                                            CGFloat expected = expectedSize;
-                                            CGFloat progress = (expectedSize == 0) ? 0 : (received / expected);
-                                            downloadProgress(progress);
-                                        });
-                                    }
-                                   transform:nil
-                                  completion:^(UIImage * _Nullable image, NSURL * _Nonnull url, YYWebImageFromType from, YYWebImageStage stage, NSError * _Nullable error) {
-                                      completion(image, error, operation);
-                                  }];
-    return operation;
+    @autoreleasepool {
+        YYWebImageManager *manager = [YYWebImageManager sharedManager];
+        __weak YYWebImageOperation *operation = nil;
+        operation = [manager requestImageWithURL:URL
+                                         options:YYWebImageOptionSetImageWithFadeAnimation
+                                        progress:^(NSInteger receivedSize, NSInteger expectedSize) {
+                                            dispatch_async(callbackQueue, ^{
+                                                CGFloat received = receivedSize;
+                                                CGFloat expected = expectedSize;
+                                                CGFloat progress = (expectedSize == 0) ? 0 : (received / expected);
+                                                downloadProgress(progress);
+                                            });
+                                        }
+                                       transform:nil
+                                      completion:^(UIImage * _Nullable image, NSURL * _Nonnull url, YYWebImageFromType from, YYWebImageStage stage, NSError * _Nullable error) {
+                                          completion(image, error, operation);
+                                      }];
+        return operation;
+    }
 }
 
 - (void)cancelImageDownloadForIdentifier:(id)downloadIdentifier
@@ -41,25 +43,26 @@
 
 - (void)cachedImageWithURL:(NSURL *)URL callbackQueue:(dispatch_queue_t)callbackQueue completion:(ASImageCacherCompletion)completion
 {
-    UIImage *image = [[YYImageCache sharedCache] getImageForKey:URL.absoluteString];
-    if (image) {
-        dispatch_async(callbackQueue, ^{
-            completion(image);
-        });
-    }else {
-        dispatch_async(callbackQueue, ^{
-            [self downloadImageWithURL:URL
-                         callbackQueue:callbackQueue
-                      downloadProgress:^(CGFloat progress) {
-                          
-                      }
-                            completion:^(id<ASImageContainerProtocol>  _Nullable image, NSError * _Nullable error, id  _Nullable downloadIdentifier) {
-                                if (image) {
-                                    completion(image);
-                                }
-                            }];
-        });
-    }
+    [[YYImageCache sharedCache] getImageForKey:URL.absoluteString withType:YYImageCacheTypeDisk withBlock:^(UIImage * _Nullable image, YYImageCacheType type) {
+        if (image) {
+            dispatch_async(callbackQueue, ^{
+                completion(image);
+            });
+        }else {
+            dispatch_async(callbackQueue, ^{
+                [self downloadImageWithURL:URL
+                             callbackQueue:callbackQueue
+                          downloadProgress:^(CGFloat progress) {
+                              
+                          }
+                                completion:^(id<ASImageContainerProtocol>  _Nullable image, NSError * _Nullable error, id  _Nullable downloadIdentifier) {
+                                    if (image) {
+                                        completion(image);
+                                    }
+                                }];
+            });
+        }
+    }];
 }
 
 @end
